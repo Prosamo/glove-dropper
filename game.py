63d2767973bafc4acf80bf25f7256e47fe996abb
Prosamo <s4lm0n.014@gmail.com>
2024-11-17 22:24:53 +0900

    Add files via upload

--- game.py
+++ game.py
@@ -1,10 +1,8 @@
-import pyxel, ctypes
-#import PyxelUniversalFont as puf
+import pyxel
 
 score = 0
 money = 0
 watched = False
-#writer = puf.Writer("ipa_gothic.ttf")
 
 class Title:
     def update(self):
@@ -17,31 +15,15 @@ class Title:
         elif pyxel.btnp(pyxel.GAMEPAD1_BUTTON_B):
             state.set(Game())
     def draw(self):
-        pyxel.cls(7)
-        #writer.draw(352, 16, 'バイトマニュアル', 16, 5)
-        #writer.draw(32, 16, '軍手を落とすだけ！　手軽に高収入！', 16, pyxel.COLOR_BLACK)
-        #writer.draw(32, 48, '超ホワイト！　時給最高3000円！', 16, pyxel.COLOR_BLACK)
-        #if watched:
-        #    writer.draw(272, 160, '...そんなバイトはありません')
-        #writer.draw(64, 96, '軍手落とし', 48, pyxel.COLOR_RED)
-        #writer.draw(160, 208, '-PRESS ENTER TO START-', 16, pyxel.COLOR_BLACK)
+        # 画像を描画する（画面の座標(0, 0)に描画）
+        pyxel.blt(0, 0, title_image, 0, 0, 512, 256)
 
 class Help:
     def update(self):
         if pyxel.btnp(pyxel.KEY_RETURN) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_B):
             state.set(Title())
     def draw(self):
-        pyxel.cls(7)
-        #writer.draw(16, 16, '仕事内容', 16, pyxel.COLOR_BLACK)
-        #writer.draw(32, 48, '　軍手を投げて、街中のコインを集めます。落下中の軍手で', 16, pyxel.COLOR_BLACK)
-        #writer.draw(32, 80, 'ないと、上手くコインが入らないので注意してください。', 16, pyxel.COLOR_BLACK)
-        #writer.draw(32, 112, '前投げ　　　→　F', 16, pyxel.COLOR_BLACK)
-        #writer.draw(32, 144, '落とす　　　→　D', 16, pyxel.COLOR_BLACK)
-        #writer.draw(32, 176, '後ろ投げ　　→　S', 16, pyxel.COLOR_BLACK)
-        #writer.draw(288, 112, 'ジャンプ　　→　スペース', 16, pyxel.COLOR_BLACK)
-        #writer.draw(288, 144, 'リスタート　→　エンター', 16, pyxel.COLOR_BLACK)
-        #writer.draw(288, 176, 'タイトルへ　→　Q', 16, pyxel.COLOR_BLACK)
-        #writer.draw(160, 208, '-PRESS ENTER TO CLOSE-', 16, pyxel.COLOR_BLACK)
+        pyxel.blt(0, 0, help_image, 0, 0, 512, 256)
 
 class Result:
     def update(self):
@@ -54,11 +36,35 @@ class Result:
         if pyxel.btnp(pyxel.KEY_Q):
             state.set(Title())
     def draw(self):
-        pyxel.cls(0)
-        #writer.draw(32, 64, f'？？？「今回の報酬は{score*100}円だ...受け取れ...」', 16, pyxel.COLOR_WHITE)
-        #writer.draw(256, 160, f'これまでに稼いだ金額　{money}円', 16, pyxel.COLOR_WHITE)
-        #writer.draw(160, 192, '-PRESS ENTER TO RESTART-', 16, pyxel.frame_count % 16)
+        pyxel.blt(0, 0, result_image, 0, 0, 512, 256)
+        n = score // 10
+        n2 = score % 10
+        if n:
+            pyxel.blt(192, 64, 0, 8*n, 80, 8, 16, 0)
+            pyxel.blt(200, 64, 0, 8*n2, 80, 8, 16, 0)
+            pyxel.blt(208, 64, 0, 0, 80, 8, 16, 0)
+        elif n2:
+            pyxel.blt(200, 64, 0, 8*n2, 80, 8, 16, 0)
+            pyxel.blt(208, 64, 0, 0, 80, 8, 16, 0)
+        pyxel.blt(216, 64, 0, 0, 80, 8, 16, 0)
 
+        n = money//10000
+        n2 = money // 1000 % 10
+        n3 = money //100 % 10
+        if n:
+            pyxel.blt(430, 160, 0, 8*n, 80, 8, 16, 0)
+            pyxel.blt(438, 160, 0, 8*n2, 80, 8, 16, 0)
+            pyxel.blt(446, 160, 0, 8*n3, 80, 8, 16, 0)
+            pyxel.blt(454, 160, 0, 0, 80, 8, 16, 0)
+        elif n2:
+            pyxel.blt(438, 160, 0, 8*n2, 80, 8, 16, 0)
+            pyxel.blt(446, 160, 0, 8*n3, 80, 8, 16, 0)
+            pyxel.blt(454, 160, 0, 0, 80, 8, 16, 0)
+        elif n3:
+            pyxel.blt(446, 160, 0, 8*n3, 80, 8, 16, 0)
+            pyxel.blt(454, 160, 0, 0, 80, 8, 16, 0)
+        pyxel.blt(462, 160, 0, 0, 80, 8, 16, 0)
+        
 class Event:
     def __init__(self):
         global watched
@@ -87,12 +93,8 @@ class Event:
         if self.darkness:
             pyxel.rect(0, 0, pyxel.width, int(self.darkness), 0)
         #暗転が終わっていたら追加で文字を描画
-        #if self.darkness >= pyxel.height:
-        #    writer.draw(32, 32, 'BAD END...', 48, pyxel.COLOR_RED)
-        #    writer.draw(16, 144, '逮捕されてしまった...', 16, pyxel.COLOR_WHITE)
-        #    writer.draw(16, 160, 'あんな怪しいバイトに手を出していなければ...', 16, pyxel.COLOR_WHITE)
-        #    writer.draw(160, 224, '-PRESS ENTER TO TITLE-', 16, pyxel.frame_count % 16)
-            
+        if self.darkness >= pyxel.height:
+            pyxel.blt(0, 0, end_image, 0, 0, 512, 256)
 
 class Game:
     def __init__(self):
@@ -210,7 +212,13 @@ class Game:
         pyxel.bltm(0, 0, 0, 0, 0, 512, 64)    #空の部分だけ固定
         pyxel.bltm(self.bg_x, 64, 0, 0, 64, 512, 192)    #background
         pyxel.bltm(self.bg2_x, 64, 0, 0, 64, 512, 192)    #background2
-        #writer.draw(8, 8, f'Gloves: {self.glove_count}', 16, pyxel.COLOR_RED)
+
+        #軍手の残機
+        n = self.glove_count // 10
+        n2 = self.glove_count % 10
+        if n:
+            pyxel.blt(104, 16, 0, 8*n, 80, 8, 16, 0)
+        pyxel.blt(112, 16, 0, 8*n2, 80, 8, 16, 0)
         frame = (pyxel.frame_count // 5) % 4  # 3フレームのアニメーション
         #ジャンプの間は画像を固定
         if self.is_jumping:
@@ -234,8 +242,17 @@ class State:
         self.state = state
 class App:
     def __init__(self):
+        global title_image, help_image, end_image, result_image
         pyxel.init(512, 256, title="軍手落とし")
         pyxel.load('sample.pyxres')
+        title_image = pyxel.Image(512, 256)
+        title_image.load(0, 0, "title.png")
+        help_image = pyxel.Image(512, 256)
+        help_image.load(0, 0, "help.png")
+        end_image = pyxel.Image(512, 256)
+        end_image.load(0, 0, "end.png")
+        result_image = pyxel.Image(512, 256)
+        result_image.load(0, 0, "result.png")
         pyxel.run(self.update, self.draw)
         
     def update(self):
