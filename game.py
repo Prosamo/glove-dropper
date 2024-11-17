79781a0365521455c627544bb9c0c843ad1d8604
Prosamo <s4lm0n.014@gmail.com>
2024-11-17 13:45:09 +0900

    Update game.py

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 import pyxel
-import PyxelUniversalFont as puf
+#import PyxelUniversalFont as puf
 score = 0
 money = 0
 watched = False
-writer = puf.Writer("ipa_gothic.ttf")
+#writer = puf.Writer("ipa_gothic.ttf")
 
 class Button:
     def __init__(self, x, y, w, h, color = 7, border_color = 0, font_color = 0, mark = ''):
@@ -19,7 +19,7 @@ class Button:
         pyxel.elli(self.x, self.y, self.w, self.h, self.color)
         pyxel.ellib(self.x, self.y, self.w, self.h, self.border_color)
         
-        writer.draw(self.x + self.w//2 -8, self.y + self.h//2 -8, self.mark, 16, self.font_color)
+        #writer.draw(self.x + self.w//2 -8, self.y + self.h//2 -8, self.mark, 16, self.font_color)
     def pressed(self, mouse_x, mouse_y):
         if (self.x <= mouse_x <= self.x + self.w and self.y <= mouse_y <= self.y + self.h):
             return True
@@ -39,13 +39,13 @@ class Title:
             state.set(Help())
     def draw(self):
         pyxel.cls(7)
-        writer.draw(352, 16, 'バイトマニュアル', 16, 5)
-        writer.draw(32, 16, '軍手を落とすだけ！　手軽に高収入！', 16, pyxel.COLOR_BLACK)
-        writer.draw(32, 48, '超ホワイト！　時給最高3000円！', 16, pyxel.COLOR_BLACK)
+        #writer.draw(352, 16, 'バイトマニュアル', 16, 5)
+        #writer.draw(32, 16, '軍手を落とすだけ！　手軽に高収入！', 16, pyxel.COLOR_BLACK)
+        #writer.draw(32, 48, '超ホワイト！　時給最高3000円！', 16, pyxel.COLOR_BLACK)
         if watched:
-            writer.draw(272, 160, '...そんなバイトはありません')
-        writer.draw(64, 96, '軍手落とし', 48, pyxel.COLOR_RED)
-        writer.draw(160, 208, '-PRESS ENTER TO START-', 16, pyxel.COLOR_BLACK)
+            #writer.draw(272, 160, '...そんなバイトはありません')
+        #writer.draw(64, 96, '軍手落とし', 48, pyxel.COLOR_RED)
+        #writer.draw(160, 208, '-PRESS ENTER TO START-', 16, pyxel.COLOR_BLACK)
 
 class Help:
     def update(self):
@@ -53,16 +53,16 @@ class Help:
             state.set(Title())
     def draw(self):
         pyxel.cls(7)
-        writer.draw(16, 16, '仕事内容', 16, pyxel.COLOR_BLACK)
-        writer.draw(32, 48, '　軍手を投げて、街中のコインを集めます。落下中の軍手で', 16, pyxel.COLOR_BLACK)
-        writer.draw(32, 80, 'ないと、上手くコインが入らないので注意してください。', 16, pyxel.COLOR_BLACK)
-        writer.draw(32, 112, '前投げ　　　→　F', 16, pyxel.COLOR_BLACK)
-        writer.draw(32, 144, '落とす　　　→　D', 16, pyxel.COLOR_BLACK)
-        writer.draw(32, 176, '後ろ投げ　　→　S', 16, pyxel.COLOR_BLACK)
-        writer.draw(288, 112, 'ジャンプ　　→　スペース', 16, pyxel.COLOR_BLACK)
-        writer.draw(288, 144, 'リスタート　→　エンター', 16, pyxel.COLOR_BLACK)
-        writer.draw(288, 176, 'タイトルへ　→　Q', 16, pyxel.COLOR_BLACK)
-        writer.draw(160, 208, '-PRESS ENTER TO CLOSE-', 16, pyxel.COLOR_BLACK)
+        #writer.draw(16, 16, '仕事内容', 16, pyxel.COLOR_BLACK)
+        #writer.draw(32, 48, '　軍手を投げて、街中のコインを集めます。落下中の軍手で', 16, pyxel.COLOR_BLACK)
+        #writer.draw(32, 80, 'ないと、上手くコインが入らないので注意してください。', 16, pyxel.COLOR_BLACK)
+        #writer.draw(32, 112, '前投げ　　　→　F', 16, pyxel.COLOR_BLACK)
+        #writer.draw(32, 144, '落とす　　　→　D', 16, pyxel.COLOR_BLACK)
+        #writer.draw(32, 176, '後ろ投げ　　→　S', 16, pyxel.COLOR_BLACK)
+        #writer.draw(288, 112, 'ジャンプ　　→　スペース', 16, pyxel.COLOR_BLACK)
+        #writer.draw(288, 144, 'リスタート　→　エンター', 16, pyxel.COLOR_BLACK)
+        #writer.draw(288, 176, 'タイトルへ　→　Q', 16, pyxel.COLOR_BLACK)
+        #writer.draw(160, 208, '-PRESS ENTER TO CLOSE-', 16, pyxel.COLOR_BLACK)
 
 class Result:
     def update(self):
@@ -83,9 +83,9 @@ class Result:
             state.set(Title())
     def draw(self):
         pyxel.cls(0)
-        writer.draw(32, 64, f'？？？「今回の報酬は{score*100}円だ...受け取れ...」', 16, pyxel.COLOR_WHITE)
-        writer.draw(256, 160, f'これまでに稼いだ金額　{money}円', 16, pyxel.COLOR_WHITE)
-        writer.draw(160, 192, '-PRESS ENTER TO RESTART-', 16, pyxel.frame_count % 16)
+        #writer.draw(32, 64, f'？？？「今回の報酬は{score*100}円だ...受け取れ...」', 16, pyxel.COLOR_WHITE)
+        #writer.draw(256, 160, f'これまでに稼いだ金額　{money}円', 16, pyxel.COLOR_WHITE)
+        #writer.draw(160, 192, '-PRESS ENTER TO RESTART-', 16, pyxel.frame_count % 16)
 
 class Event:
     def __init__(self):
@@ -111,16 +111,16 @@ class Event:
         pyxel.bltm(0, 0, 0, 0, 0, 512, 256)
         pyxel.blt(128, 192, 0, 24, 0, 16, 16, 9)# Player
         pyxel.blt(144, 192, 0, 72, 0, 16, 16, 9)# Police
-        writer.draw(128, 224, '警察官「そこの君ちょっといいかな？」', 16, pyxel.COLOR_BLACK)
+        #writer.draw(128, 224, '警察官「そこの君ちょっといいかな？」', 16, pyxel.COLOR_BLACK)
         # 暗転の度合いに応じて黒い矩形を描画
         if self.darkness:
             pyxel.rect(0, 0, pyxel.width, int(self.darkness), 0)
         #暗転が終わっていたら追加で文字を描画
         if self.darkness >= pyxel.height:
-            writer.draw(32, 32, 'BAD END...', 48, pyxel.COLOR_RED)
-            writer.draw(16, 144, '逮捕されてしまった...', 16, pyxel.COLOR_WHITE)
-            writer.draw(16, 160, 'あんな怪しいバイトに手を出していなければ...', 16, pyxel.COLOR_WHITE)
-            writer.draw(160, 224, '-PRESS ENTER TO TITLE-', 16, pyxel.frame_count % 16)
+            #writer.draw(32, 32, 'BAD END...', 48, pyxel.COLOR_RED)
+            #writer.draw(16, 144, '逮捕されてしまった...', 16, pyxel.COLOR_WHITE)
+            #writer.draw(16, 160, 'あんな怪しいバイトに手を出していなければ...', 16, pyxel.COLOR_WHITE)
+            #writer.draw(160, 224, '-PRESS ENTER TO TITLE-', 16, pyxel.frame_count % 16)
             
 
 class Game:
@@ -259,7 +259,7 @@ class Game:
         pyxel.bltm(0, 0, 0, 0, 0, 512, 64)    #空の部分だけ固定
         pyxel.bltm(self.bg_x, 64, 0, 0, 64, 512, 192)    #background
         pyxel.bltm(self.bg2_x, 64, 0, 0, 64, 512, 192)    #background2
-        writer.draw(8, 8, f'Gloves: {self.glove_count}', 16, pyxel.COLOR_RED)
+        #writer.draw(8, 8, f'Gloves: {self.glove_count}', 16, pyxel.COLOR_RED)
         frame = (pyxel.frame_count // 5) % 4  # 3フレームのアニメーション
         #ジャンプの間は画像を固定
         if self.is_jumping:
